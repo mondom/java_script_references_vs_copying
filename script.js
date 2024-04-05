49d40d48dc3c2020ea2a0f1447856dff3bc1624b
Monika <codingbymon@gmail.com>
2024-04-05 15:52:58 +0200

    deep object cloning

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ const players = ['Wes', 'Sarah', 'Ryan', 'Poppy']
 
 let team = players
 
-console.log(players, team);
+console.log(players, team)
 
 team[3] = 'Maria'
-console.log(players, team);
+console.log(players, team)
 
 // dlaczego zmienia się tablica players?
 // To zjawisko wynika z faktu, że w JavaScript zmienne przechowują referencje do obiektów, a nie same obiekty. W przypadku tablic i innych obiektów złożonych, takich jak tablice, obie zmienne players i team wskazują na ten sam obiekt (tj. tę samą tablicę), więc zmiany dokonane za pomocą jednej zmiennej są widoczne przez drugą zmienną. Innymi słowy, obie zmienne players i team wskazują na ten sam obszar w pamięci, więc zmiana jednej tablicy (np. poprzez zmianę elementu) zostanie odzwierciedlona w obu zmiennych. Aby uniknąć tego zachowania, można skopiować tablicę, na którą wskazuje zmienna, np. za pomocą metody slice() lub concat()
@@ -41,8 +41,8 @@ const team3 = [].concat(players)
 // or use the new ES6 Spread
 const team4 = [...players]
 team4[1] = 'weeee'
-console.log(team4);
-console.log(players);
+console.log(team4)
+console.log(players)
 
 const team5 = Array.from(players)
 // now when we update it, the original one isn't changed
@@ -57,8 +57,45 @@ const person = {
 
 // and think we make a copy:
 
+// const captain = person
+// captain.number = 99
+// console.log(person)
+
 // how do we take a copy instead?
+const captain2 = Object.assign({}, person, { number: 99, age: 12 })
+console.log(captain2)
+console.log(person)
 
 // We will hopefully soon see the object ...spread
-
+const captain3 = { ...person }
 // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+const user = {
+	name: 'Monika',
+	age: 35,
+	social: {
+		fb: '@fb',
+		x: '@x',
+	},
+}
+console.clear()
+console.log(user)
+
+const dev = Object.assign({}, user)
+console.log(dev)
+// taki sposób kopiowania jest płytki, kopiuje tylko obiekt najwyższy rangą, a reszta pozostaje referencją do oryginalnego obiektu. Więc jeśli coś zmienimy w podległym obiekcie to zmiana będzie widoczna również w jego oryginale!↓
+// dev.social.x = '@coolWoman'
+// console.log(dev);
+// console.log(user);
+
+const dev2 = JSON.parse(JSON.stringify(user))
+console.log(JSON.stringify(user));
+console.log(typeof JSON.stringify(user));
+// to już nie obiekt, tylko string↑
+console.log(dev2);
+// ↑ głębokie klonowanie obiektu - niezalecane
+// Kod JSON.parse(JSON.stringify(user)) wykonuje głębokie klonowanie obiektu user. Oto jak to działa:
+
+// JSON.stringify(user): Konwertuje obiekt user na jego reprezentację w formacie JSON. JSON jest formatem danych tekstowych, więc w wyniku tej operacji otrzymujemy ciąg znaków zawierający wszystkie informacje z obiektu user.
+// JSON.parse(...): Następnie funkcja JSON.parse() analizuje ten ciąg znaków JSON i tworzy na jego podstawie nowy obiekt. Ponieważ ta operacja parsowania JSON i ponownego tworzenia obiektu jest głęboka, oznacza to, że obiekty zagnieżdżone w user również zostaną sklonowane.
+// Ostatecznie dev2 będzie identyczną kopią obiektu user, co oznacza, że zmiany wprowadzone do jednego z tych obiektów nie będą miały wpływu na drugi. Jest to często używane do tworzenia głębokich kopii obiektów, szczególnie gdy chcemy uniknąć referencji do tych samych danych w różnych miejscach naszego programu.
